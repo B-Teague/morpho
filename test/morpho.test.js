2d4c385d3115da9a1629237692833da5177df4b1
Brian Teague <br.teague@gmail.com>
2021-11-07 22:08:13 -0500

    Get core tests working

--- test/morpho.test.js
+++ test/morpho.test.js
@@ -1,146 +1,126 @@
-// import 'jsdom-global/register'
+import 'jsdom-global/register.js'
+import test from 'ava'
+import morpho from '../src/morpho-core/index.js'
 
-// import { equal, deepEqual } from "testmatrix"
-
-// import morpho from '../src/index'
-
-// const options = {
-//   unit: "%",
-//   cssProps: {
-//     "border-right-width": {
-//       "vendor": ["-o-"],
-//       "unit": "em"
-//     },
-//     "height": {
-//       "unit": "em"
-//     }
-//   }
-// }
+const options = {
+  unit: "%",
+  cssProps: {
+    "border-right-width": {
+      "vendor": ["-o-"],
+      "unit": "em"
+    },
+    "height": {
+      "unit": "em"
+    }
+  }
+}
 
 
-// const { css, keyframes } = morpho(options);
-// const m2 = morpho();
+const { css, keyframes } = morpho(options);
+const m2 = morpho();
 
-// export default {
-//   morpho: [
-//     { //#1
-//       name: "Create a class using the css function without prefix option",
-//       assert: equal,
-//       actual: (() => {
-//         css({ backgroundColor: "red" })
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
-//       })(),
-//       expected: ".morpho-mk4wc7 {background-color: red;}"
-//     },
-//     { //#2
-//       name: "insert vendor prefix",
-//       assert: equal,
-//       actual: (() => {
-//         css({
-//           borderRightWidth: "10px"
-//         })
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
-//       })(),
-//       expected: ".morpho-dat4zz {-o-border-right-width: 10px; border-right-width: 10px;}"
-//     },
-//     { //#3
-//       name: "Create a unique keyframes name to be used by a css class",
-//       assert: equal,
-//       actual: (() => {
-//         keyframes({
-//           from: {
-//             transform: 'scale(2)'
-//           },
-//           to: {
-//             transform: 'scale(4)'
-//           },
-//         })
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
-//       })(),
-//       expected: "@keyframes morpho-o5wy39 { \n  from {transform: scale(2);} \n  to {transform: scale(4);} \n}"
-//     },
-//     { //#4
-//       name: "create a media query",
-//       assert: deepEqual,
-//       actual: (() => {
-//         css({
-//           fontSize: "10px",
-//           "@media (max-width: 450px)": {
-//             fontSize: "32px",
-//             h1: {
-//               color: "yellow"
-//             }
-//           }
-//         });
-//         return [
-//           document.styleSheets[0].cssRules.map((rule) => rule.cssText)[1],
-//           document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
-//         ]
-//       })(),
-//       expected: [
-//         "@media (max-width: 450px) {.morpho-3kd75a h1 {color: yellow;}.morpho-3kd75a {font-size: 32px;}}",
-//         ".morpho-3kd75a {font-size: 10px;}"
-//       ]
-//     },
-//     { //#5
-//       name: "Add props to internal cssProps object",
-//       assert: equal,
-//       actual: (() => {
-//         css({
-//           borderRadius: 10,
-//         });
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
-//       })(),
-//       expected: ".morpho-pryrtz {border-radius: 10%;}"
-//     },
-//     { //#6
-//       name: "pseudo selector",
-//       assert: equal,
-//       actual: (() => {
-//         css({
-//           "&:hover": {
-//             color: "red"
-//           }
-//         })
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
+test("Create a class using the css function without prefix option", t => {
+    css({ backgroundColor: "red" })
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        ".morpho-mk4wc7 {background-color: red;}"
+    )
+})
 
-//       })(),
-//       expected: ".morpho-1l80iw9:hover {color: red;}"
-//     },
-//     { //#7
-//       name: "repeat a css property multiple times using an array of values",
-//       assert: equal,
-//       actual: (() => {
-//         css({
-//           position: [["fixed"], ["sticky"]]
-//         })
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0];
-//       })(),
-//       expected: ".morpho-v61bi1 {position: sticky;}" //! Both occurrences of position were inserted, but the browser only selects the last successful match
-//     },
-//     { //#8
-//       name: "List of values",
-//       assert: deepEqual,
-//       actual: (() => {
-//         css({
-//           "transition": ["width", "2s"]
-//         })
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
+test("insert vendor prefix", t => {
+    css({
+        borderRightWidth: "10px"
+    })
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        ".morpho-dat4zz {-o-border-right-width: 10px; border-right-width: 10px;}"
+    )
+})
 
-//       })(),
-//       expected: ".morpho-ufznj {transition: width 2s;}"
-//     },
-//     { //#9
-//       name: "Test morph without options parm",
-//       assert: deepEqual,
-//       actual: (() => {
-//         m2.css({
-//           color: "orange"
-//         })
-//         return document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
+test("Create a unique keyframes name to be used by a css class", t => {
+    keyframes({
+        from: {
+        transform: 'scale(2)'
+        },
+        to: {
+        transform: 'scale(4)'
+        },
+    })
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        "@keyframes morpho-o5wy39 { \n  from {transform: scale(2);} \n  to {transform: scale(4);} \n}"
+    )
+})
 
-//       })(),
-//       expected: ".morpho-xteix3 {color: orange;}"
-//     }
-//   ]
-// }
+test("create a media query", t => {
+    css({
+        fontSize: "10px",
+        "@media (max-width: 450px)": {
+        fontSize: "32px",
+        h1: {
+            color: "yellow"
+        }
+        }
+    });
+    t.deepEqual([
+            document.styleSheets[0].cssRules.map((rule) => rule.cssText)[1],
+            document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0]
+        ],
+        [
+            "@media (max-width: 450px) {.morpho-3kd75a h1 {color: yellow;}.morpho-3kd75a {font-size: 32px;}}",
+            ".morpho-3kd75a {font-size: 10px;}"
+        ]
+    )
+})
+
+test("Add props to internal cssProps object", t => {
+    css({
+          borderRadius: 10,
+        });
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        ".morpho-pryrtz {border-radius: 10%;}"
+    )
+})
+
+test("pseudo selector", t => {
+    css({
+        "&:hover": {
+        color: "red"
+        }
+    })
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        ".morpho-1l80iw9:hover {color: red;}"
+    )
+})
+
+test("repeat a css property multiple times using an array of values", t => {
+    css({
+        position: [["fixed"], ["sticky"]]
+    })
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        ".morpho-v61bi1 {position: sticky;}" //! Both occurrences of position were inserted, but the browser only selects the last successful match
+    )
+})
+
+test("List of values", t => {
+    css({
+        "transition": ["width", "2s"]
+    })
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        ".morpho-ufznj {transition: width 2s;}"
+    )
+})
+
+test("Test morph without options parm", t => {
+    m2.css({
+        color: "orange"
+    })
+    t.deepEqual(
+        document.styleSheets[0].cssRules.map((rule) => rule.cssText)[0],
+        ".morpho-xteix3 {color: orange;}"
+    )
+})
